cea7bf79245071019feacac859ec6cb9b7ecb55b
Nathaniel Davis <Nathanieldavis63@gmail.com>
2021-02-15 18:17:28 -0600

    Floating Point Types tutorial code.

--- FloatingPointTypes/src/FloatingPointTypes.cpp
+++ FloatingPointTypes/src/FloatingPointTypes.cpp
@@ -7,13 +7,31 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
-	int value = 6;
+	float fValue = 123.456789;
+	cout << sizeof(float) << endl;
+	cout << setprecision(20) << fixed << fValue << endl;
+	double dValue = 76.4;
+	cout << setprecision(20) << fixed << dValue << endl;
+	long double lValue = 123.456789876543210;
 
-	cout << value << endl;
+	cout << setprecision(20) << fixed << lValue << endl;
+
+	cout << sizeof(lValue) << endl;
+
+
+
+
+
+
+
+
+
+
 
 
 
