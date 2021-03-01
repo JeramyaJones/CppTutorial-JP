867fbe0d95a02aea4f6e083f7bb12a7bac7c5a88
Nathaniel Davis <Nathanieldavis63@gmail.com>
2021-03-01 00:19:18 -0600

    Function Parameters Tutorial

--- FunctionParameters/src/FunctionParameters.cpp
+++ FunctionParameters/src/FunctionParameters.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : ReturnValues.cpp
+// Name        : FunctionParameters.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -9,36 +9,39 @@
 #include <iostream>
 using namespace std;
 
-void showMenu(){
+void showMenu() {
 	cout << "1. Search" << endl;
 	cout << "2. View Record" << endl;
 	cout << "3. Quit" << endl;
 }
 
-int getInput(){
+int getInput() {
 	cout << "Enter selection: " << flush;
 	int input;
 	cin >> input;
 	return input;
 }
 
+void processSelection(int selection) {
+	switch (selection) {
+	case 1:
+		cout << "Searching ..." << endl;
+		break;
+	case 2:
+		cout << "Viewing ..." << endl;
+		break;
+	case 3:
+		cout << "Quitting ..." << endl;
+		break;
+	default:
+		cout << "Please select an item from the menu." << endl;
+	}
+
+}
+
 int main() {
 	showMenu();
 	int selection = getInput();
-
-	switch(selection){
-		case 1:
-			cout << "Searching ..." << endl;
-			break;
-		case 2:
-			cout << "Viewing ..." << endl;
-			break;
-		case 3:
-			cout << "Quitting ..." << endl;
-			break;
-		default:
-			cout << "Please select an item from the menu." << endl;
-		}
-
+	processSelection(selection);
 	return 0;
 }
